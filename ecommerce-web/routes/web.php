1cd59105bd682d1500d9b0bb5fd1cbf664b45557
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-06-09 20:24:22 +0700

    thêm thư viện và set up trang login và admin

--- ecommerce-web/routes/web.php
+++ ecommerce-web/routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/admin', function () {
+    return view('admin.auth.login');
+});
+
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('admin/admin/list', function () {
+    return view('admin.admin.list');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
